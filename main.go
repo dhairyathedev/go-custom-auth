e153ea635100aec89f332a6900cc70c16314cce3
Dhairya Shah <hello@dhairyashah.dev>
2025-05-04 18:51:20 +0530

    update: implemented post request for signup (incomplete)

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	//"database/sql"
+	"database/sql"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
+
+	"log"
+	"time"
 )
+
+type User struct {
+	ID           string    `db:"id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
+}
 
 func main() {
 	r := gin.Default()
@@ -14,19 +26,37 @@ func main() {
 		})
 	})
 
-	r.POST("/signup", func(c *gin.Context) {
-		password := []byte("hello@123")
-		hash, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": err,
-			})
-		}
-		c.JSON(200, gin.H{
-			"message": "Successfully Signed up!!!",
-			"hash":    hash,
-		})
-	})
+	r.POST("/signup", signup)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func signup(c *gin.Context) {
+
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Error hashing the password",
+		})
+	}
+
+	user := User{
+		Email:        input.Email,
+		PasswordHash: string(hash),
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Successfully Signed up!!!",
+		"hash":    hash,
+	})
 }
